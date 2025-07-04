20387e90bb8a1116ee2d9c5cb4aa44199559685c
Taras <rohovets.taras@gmail.com>
2025-07-04 20:40:21 +0100

    deploy 8

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
- // output: 'export',
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
+  env: {
+    CUSTOM_API_URL: process.env.CUSTOM_API_URL,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
